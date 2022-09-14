dfd43bb4275e61768dcc26bf6a4aee53c5549cd2
Phillip O'Reggio <pnoreggio@gmail.com>
2022-09-13 22:05:05 -0400

    working binary clock

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -69,25 +69,86 @@ buttonB = digitalio.DigitalInOut(board.D24)
 buttonA.switch_to_input()
 buttonB.switch_to_input()
 # Colors
-colorOff = "#000000"
-colorOn = "#00ffff"
+color_off = "#000000"
+color_on = "#00ffff"
 
 
 # ===== Helper Functions for drawing to the screen =====
 Rectangle = tuple[float, float, float, float]
-RectangleColorPair = tuple[rectangle, str]
+RectangleColorPair = tuple[Rectangle, str]
 
 
 def hour_rectangles(hour: int) -> list[Rectangle]:
-    pass
+    return rectangles_for_number(hour)
 
 
 def minute_rectangles(minute: int) -> list[Rectangle]:
-    pass
+    shift_amount = width / 3
+    rects = map(lambda r: shift_right(r, shift_amount), rectangles_for_number(minute))
+    return list(rects)
 
 
 def second_rectangles(second: int) -> list[Rectangle]:
-    pass
+    shift_amount = (2 * width) / 3
+    rects = map(lambda r: shift_right(r, shift_amount), rectangles_for_number(second))
+    return list(rects)
+
+
+def rectangles_for_number(number: int) -> list[Rectangle]:
+    assert number < 100
+    assert number >= 0
+
+    rectangles: list[Rectangle] = []
+    rectangle_width = (width / 3) / 2
+    rectangle_height = height / 4
+    base_rect = [0, 0, rectangle_width, rectangle_height]
+
+    # 10s
+    left_digit = int(number / 10)
+    if left_digit >= 8:
+        left_digit -= 8
+        rectangles += [base_rect]
+    if left_digit >= 4:
+        left_digit -= 4
+        rectangles += [shift_down(base_rect, rectangle_height)]
+    if left_digit >= 2:
+        left_digit -= 2
+        rectangles += [shift_down(base_rect, rectangle_height * 2)]
+    if left_digit >= 1:
+        left_digit -= 1
+        rectangles += [shift_down(base_rect, rectangle_height * 3)]
+    # 1s
+    right_digit = int(number % 10)
+    if right_digit >= 8:
+        right_digit -= 8
+        rectangles += [shift_right(base_rect, rectangle_width)]
+    if right_digit >= 4:
+        right_digit -= 4
+        rectangles += [
+            shift_right(shift_down(base_rect, rectangle_height), rectangle_width)
+        ]
+    if right_digit >= 2:
+        right_digit -= 2
+        rectangles += [
+            shift_right(shift_down(base_rect, rectangle_height * 2), rectangle_width)
+        ]
+    if right_digit >= 1:
+        right_digit -= 1
+        rectangles += [
+            shift_right(shift_down(base_rect, rectangle_height * 3), rectangle_width)
+        ]
+    # return the results
+    return rectangles
+
+
+def shift_right(rect: Rectangle, amount) -> Rectangle:
+    left, bot, right, top = rect
+    return [left + amount, bot, right + amount, top]
+
+
+def shift_down(rect: Rectangle, amount) -> Rectangle:
+    left, bot, right, top = rect
+    return [left, bot + amount, right, top + amount]
 
 
 def calculate_rectangle_color_pairs(
@@ -97,16 +158,19 @@ def calculate_rectangle_color_pairs(
     pass
 
 
-def draw_rectangles(rectangleColorPairs: tuple[Rectangle, str]):
-    pass
+def draw_rectangles(rectangle_color_pairs: list[tuple[Rectangle, str]]):
+    for pair in rectangle_color_pairs:
+        rect, color = pair
+        draw.rectangle(rect, outline=0, fill=color)
 
 
 # ===== Helper Functions for working with the time
 HoursMinutesSecondsMicro = list[int, int, int, int]
 
 
-def getTimeInPieces() -> HoursMinutesSecondsMicro:
-    return datetime.now().strftime("%H:%M:%S:%f").split(":")
+def get_time_in_pieces() -> HoursMinutesSecondsMicro:
+    time_pieces = datetime.now().strftime("%H:%M:%S:%f").split(":")
+    return list(map(lambda s: int(s), time_pieces))
 
 
 while True:
@@ -115,8 +179,16 @@ while True:
 
     # =============
     # binary clock
+    hours, minutes, secs, micro = get_time_in_pieces()
+    rects = hour_rectangles(hours)
+    rects += minute_rectangles(minutes)
+    rects += second_rectangles(secs)
 
-    print(getTimeInPieces())
+    rect_pairs = list(map(lambda r: (r, color_on), rects))
+
+    draw_rectangles(rect_pairs)
+
+    # print(rectangles_for_number(22))
 
     # Display image.
     disp.image(image, rotation)
