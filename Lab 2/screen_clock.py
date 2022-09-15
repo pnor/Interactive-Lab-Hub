aa13ebd4b0f041c5bb46ce869de34f872e20e081
Phillip O'Reggio <pno3@cornell.edu>
2022-09-15 09:30:22 -0400

    add labels

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -1,7 +1,9 @@
-import time
-import subprocess
-import digitalio
 import board
+import digitalio
+import subprocess
+import time
+from datetime import datetime
+import typing
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
 
@@ -60,19 +62,145 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+# Setup constants and related
+# Buttons
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+# Colors
+color_off = "#000000"
+color_on = "#00ffff"
+
+
+# ===== Helper Functions for drawing to the screen =====
+Rectangle = tuple[float, float, float, float]
+RectangleColorPair = tuple[Rectangle, str]
+
+
+def hour_rectangles(hour: int) -> list[Rectangle]:
+    return rectangles_for_number(hour)
+
+
+def minute_rectangles(minute: int) -> list[Rectangle]:
+    shift_amount = width / 3
+    rects = map(lambda r: shift_right(r, shift_amount), rectangles_for_number(minute))
+    return list(rects)
+
+
+def second_rectangles(second: int) -> list[Rectangle]:
+    shift_amount = (2 * width) / 3
+    rects = map(lambda r: shift_right(r, shift_amount), rectangles_for_number(second))
+    return list(rects)
+
+
+def rectangles_for_number(number: int) -> list[Rectangle]:
+    assert number < 100
+    assert number >= 0
+
+    rectangles: list[Rectangle] = []
+    rectangle_width = (width / 3) / 2
+    rectangle_height = height / 4
+    base_rect = [0, 0, rectangle_width, rectangle_height]
+
+    # 10s
+    left_digit = int(number / 10)
+    if left_digit >= 8:
+        left_digit -= 8
+        rectangles += [base_rect]
+    if left_digit >= 4:
+        left_digit -= 4
+        rectangles += [shift_down(base_rect, rectangle_height)]
+    if left_digit >= 2:
+        left_digit -= 2
+        rectangles += [shift_down(base_rect, rectangle_height * 2)]
+    if left_digit >= 1:
+        left_digit -= 1
+        rectangles += [shift_down(base_rect, rectangle_height * 3)]
+    # 1s
+    right_digit = int(number % 10)
+    if right_digit >= 8:
+        right_digit -= 8
+        rectangles += [shift_right(base_rect, rectangle_width)]
+    if right_digit >= 4:
+        right_digit -= 4
+        rectangles += [
+            shift_right(shift_down(base_rect, rectangle_height), rectangle_width)
+        ]
+    if right_digit >= 2:
+        right_digit -= 2
+        rectangles += [
+            shift_right(shift_down(base_rect, rectangle_height * 2), rectangle_width)
+        ]
+    if right_digit >= 1:
+        right_digit -= 1
+        rectangles += [
+            shift_right(shift_down(base_rect, rectangle_height * 3), rectangle_width)
+        ]
+    # return the results
+    return rectangles
+
+
+def shift_right(rect: Rectangle, amount) -> Rectangle:
+    left, bot, right, top = rect
+    return [left + amount, bot, right + amount, top]
+
+
+def shift_down(rect: Rectangle, amount) -> Rectangle:
+    left, bot, right, top = rect
+    return [left, bot + amount, right, top + amount]
+
+
+def calculate_rectangle_color_pairs(
+    startRects: list[Rectangle], endRects: list[Rectangle], progress: float
+) -> RectangleColorPair:
+    """Figure out the lerping from start to end rectangles"""
+    # TODO: lotta work, mainly have to do some color arithmetic and convert it backh
+    pass
+
+
+def draw_rectangles(rectangle_color_pairs: list[tuple[Rectangle, str]]):
+    for pair in rectangle_color_pairs:
+        rect, color = pair
+        draw.rectangle(rect, outline=0, fill=color)
+
+
+# ===== Helper Functions for working with the time
+HoursMinutesSecondsMicro = list[int, int, int, int]
+
+# ===== Drawing text labels
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 12)
+
+def draw_labels(width: int, height: int):
+    # digit significance
+    for i in range(0, 6):
+        draw.text(((width / 12) + (i * (width / 6)), 5), text=('10' if i % 2 == 0 else '1'), font=font, fill="#ffdd00")
+    # binary values
+    for i in range(0, 4):
+        draw.text((5, height - (height / 8) - (i * (height / 4))), text=str(2**i), font=font, fill="#ffdd00")
+
+def get_time_in_pieces() -> HoursMinutesSecondsMicro:
+    time_pieces = datetime.now().strftime("%H:%M:%S:%f").split(":")
+    return list(map(lambda s: int(s), time_pieces))
+
+
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    # TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py
-    cur_time = time.time()
-    print(cur_time)
-    if int(cur_time) % 2 == 0:
-        print("a")
-        draw.rectangle((0, 0, width, height), outline=0, fill="#FF00FF")
-    else:
-        print("b")
-        draw.rectangle((0, 0, width, height), outline=0, fill="#00FFFF")
+    # =============
+    # binary clock
+    hours, minutes, secs, micro = get_time_in_pieces()
+    rects = hour_rectangles(hours)
+    rects += minute_rectangles(minutes)
+    rects += second_rectangles(secs)
+
+    rect_pairs = list(map(lambda r: (r, color_on), rects))
+
+    draw_rectangles(rect_pairs)
+    draw_labels(width, height)
+
+    # print(rectangles_for_number(22))
 
     # Display image.
     disp.image(image, rotation)
