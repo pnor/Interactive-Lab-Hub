0f79d57b230bf668ebc543d8218ae4e345ef969b
Phillip O'Reggio <pnoreggio@gmail.com>
2022-11-14 23:07:38 -0500

    final readme start

---
+++ Lab 6/student.py
@@ -0,0 +1,128 @@
+import paho.mqtt.client as mqtt
+import uuid
+import json
+
+# graphics lib
+import time
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+
+# ===== Graphics stuff ====================
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+# ===== End of Graphics stuff ====================
+
+topic = 'IDD/teacher_student'
+
+# some other examples
+# topic = 'IDD/a/fun/topic'
+
+def say_stars_on_screen(name: str, num_stars: int):
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    msg1 = name
+    msg2 = str(num_stars) + '/ 5'
+    y = top
+    draw.text((x, y), msg1, font=font, fill="#FFFFFF")
+    y += font.getsize(msg1)[1]
+    draw.text((x, y), msg2, font=font, fill="#FFFFFF")
+
+#this is the callback that gets called once we connect to the broker. 
+#we should add our subscribe functions here as well
+def on_connect(client, userdata, flags, rc):
+    print(f"connected with result code {rc}")
+    client.subscribe(topic)
+    # you can subsribe to as many topics as you'd like
+    # client.subscribe('some/other/topic')
+
+
+# this is the callback that gets called each time a message is recived
+def on_message(client, userdata, msg):
+    # print(f"topic: {msg.topic} msg: {msg.payload.decode('UTF-8')}")
+    if msg.topic == topic:
+        try:
+            response = json.loads(msg.payload.decode('UTF-8'))
+            print(response)
+            say_stars_on_screen(response['name'], response['stars'])
+        except e:
+            print(f"couldn't parse the response")
+
+    # you can filter by topics
+    # if msg.topic == 'IDD/some/other/topic': do thing
+
+
+# Every client needs a random ID
+client = mqtt.Client(str(uuid.uuid1()))
+# configure network encryption etc
+client.tls_set()
+# this is the username and pw we have setup for the class
+client.username_pw_set('idd', 'device@theFarm')
+
+# attach out callbacks to the client
+client.on_connect = on_connect
+client.on_message = on_message
+
+#connect to the broker
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+# this is blocking. to see other ways of dealing with the loop
+#  https://www.eclipse.org/paho/index.php?page=clients/python/docs/index.php#network-loop
+client.loop_forever()
---
+++ Lab 6/teacher.py
@@ -0,0 +1,33 @@
+import paho.mqtt.client as mqtt
+import uuid
+import json
+
+# Every client needs a random ID
+client = mqtt.Client(str(uuid.uuid1()))
+# configure network encryption etc
+client.tls_set()
+# this is the username and pw we have setup for the class
+client.username_pw_set('idd', 'device@theFarm')
+
+#connect to the broker
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+topic = "IDD/teacher_student"
+
+# stars cap at 6
+student_dict = {}
+student_dict['name'] = 'student'
+student_dict['stars'] = 0
+
+while True:
+        print(f"now writing to topic {topic}")
+        print("type new-topic to swich topics")
+        while True:
+            val = input(">> press anything to add one star to student, press q to quit: ")
+            if val =='q':
+                exit(0)
+            else:
+                student_dict['stars'] = (student_dict['stars'] + 1) % 6
+                client.publish(topic, json.dumps(student_dict))
